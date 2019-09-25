70e35de4013be32d0f1c75afe73331bd84c29f37
Aleksandr Volynec <avolinec@gmail.com>
2019-09-25 09:17:53 +0300

    Revert "Add PSR-0 autoloader"
    This reverts commit 6d5e6df1

--- src/script.php
+++ src/script.php
@@ -2,12 +2,9 @@
 
 use Receiver\Metrics;
 
-if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    require 'autoload.php';
-} else {
-    require __DIR__ . '/../vendor/autoload.php';
-}
+require __DIR__ . '/../vendor/autoload.php';
 
 $receiver = new Metrics();
 
+// Return JSON string with result
 echo $receiver->fetch() . PHP_EOL;
